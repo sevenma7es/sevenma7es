e6e172cb22dc58ec422b5ed142f34395dbb434fe
Matias Viaggio <matiasviaggio59@gmail.com>
2024-08-21 17:59:29 -0300

    Tarjetas de productos - v1.1.1

--- src/routes/client/client.router.js
+++ src/routes/client/client.router.js
@@ -101,7 +101,7 @@ clientRouter.get("/productos", userMiddleware, async (req, res) => {
     const description = "Listado de todos nuestros productos.";
     const screen = "products";
     const query = req.query.query || "";
-    const limit = req.query.limit || 9;
+    const limit = req.query.limit || 12;
     const page = req.query.page || 1;
     const sort = req.query.sort || null;
     const response = await productsController.getProducts(
@@ -210,7 +210,7 @@ clientRouter.get(
       const description = "Listado de todos nuestros productos.";
       const screen = "products";
       const keywords = req.params.keywords || "";
-      const limit = req.query.limit || 10;
+      const limit = req.query.limit || 12;
       const page = req.query.page || 1;
       const sort = req.query.sort || null;
       const response = await productsController.findByKeywords(
@@ -273,7 +273,7 @@ clientRouter.get(
       const description = "Listado de todos nuestros productos.";
       const screen = "products";
       const slug = req.params.slug || "";
-      const limit = req.query.limit || 10;
+      const limit = req.query.limit || 12;
       const page = req.query.page || 1;
       const sort = req.query.sort || null;
       const response = await productsController.findByCategory(
